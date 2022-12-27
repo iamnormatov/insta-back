d0a5f42a9864d08967cd44871802f451f66370d1
New-Bucket <azizjonnormatov437@gmail.com>
2022-12-27 21:37:52 +0500

    post

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const postCtrl = require('../controllers/postCtrl');
 const auth = require('../middleware/auth');
-
+//.
 router.route('/posts')
     .post(auth, postCtrl.createPost)
     .get(auth, postCtrl.getPosts);
