ce115f2d3d9f01fa5a398bc0cfb5c672973a88ac
New-Bucket <azizjonnormatov437@gmail.com>
2022-12-27 21:37:30 +0500

    message

--- routes/messageRouter.js
+++ routes/messageRouter.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const messageCtrl = require('../controllers/messageCtrl');
 const auth = require('../middleware/auth')
-
+//.
 router.post('/message', auth, messageCtrl.createMessage);
 
 router.get('/conversations', auth, messageCtrl.getConversations);
