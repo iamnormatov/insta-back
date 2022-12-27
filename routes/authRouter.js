3b3808a985279f17e78267775a48790ff66bd2d8
New-Bucket <azizjonnormatov437@gmail.com>
2022-12-27 21:37:00 +0500

    auth

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const authCtrl = require('../controllers/authCtrl');
-
+//.
 router.post('/register', authCtrl.register);
 router.post('/login', authCtrl.login);
 router.post('/logout', authCtrl.logout);
