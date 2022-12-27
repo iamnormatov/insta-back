49670cda9d18a4ac8cb9f51904d2ab7528e6e960
New-Bucket <azizjonnormatov437@gmail.com>
2022-12-27 21:37:43 +0500

    notify

--- routes/notifyRouter.js
+++ routes/notifyRouter.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const auth = require('../middleware/auth');
 const notifyCtrl = require('../controllers/notifyCtrl');
-
+//.
 router.post('/notify', auth, notifyCtrl.createNotify);
 
 router.delete('/notify/:id', auth, notifyCtrl.removeNotify);
