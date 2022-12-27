3f867e467e86d42d68cd16296074c80cf3160726
New-Bucket <azizjonnormatov437@gmail.com>
2022-12-27 21:37:12 +0500

    comment

--- routes/commentRouter.js
+++ routes/commentRouter.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const commentCtrl = require('../controllers/commentCtrl');
 const auth = require('../middleware/auth');
-
+//.
 router.post('/comment', auth, commentCtrl.createComment);
 router.patch('/comment/:id', auth, commentCtrl.updateComment);
 router.patch('/comment/:id/like', auth, commentCtrl.likeComment);
