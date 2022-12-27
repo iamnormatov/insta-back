07c77f016f72d2464640e2d6c2a5a720c884e5f4
New-Bucket <azizjonnormatov437@gmail.com>
2022-12-27 21:25:57 +0500

    auth

--- controllers/authCtrl.js
+++ controllers/authCtrl.js
@@ -1,7 +1,7 @@
 const Users = require('../models/userModel');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-
+//.
 const authCtrl = {
     register: async (req, res) => {
         try {
