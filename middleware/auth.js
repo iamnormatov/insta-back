2b95619f72e599726cdc03721af2d080febf04f1
New-Bucket <azizjonnormatov437@gmail.com>
2022-12-27 21:27:59 +0500

    auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,6 @@
 const Users = require('../models/userModel');
 const jwt = require('jsonwebtoken');
-
+//.
 const auth = async (req, res, next) => {
     try {
         const token = req.header("Authorization");
